f1f3f1af52cea4a5d2622e6579e8810f136338f8
KHegedus1 <kico.kiki@gmail.com>
2022-04-23 16:40:23 +0200

    varijable,promjena textova klikom na gumb

--- app/src/main/java/com/example/prvaaplikacijakh/MainActivity.kt
+++ app/src/main/java/com/example/prvaaplikacijakh/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.prvaaplikacijakh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCLick = findViewById<Button>(R.id.myButton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var
+        btnCLick.setOnClickListener{
+            btnCLick.text="Pozdrav,klik2"
+            tvMyTextView.text ="Klik2,pozdrav"
+        }
+
     }
-}
+    }
