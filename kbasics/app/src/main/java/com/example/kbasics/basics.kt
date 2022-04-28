7a40574c82b19c93d5e4cab3f08f95bee23fc270
KHegedus1 <kico.kiki@gmail.com>
2022-04-28 23:36:53 +0200

    prepravak imena,basics

---
+++ kbasics/app/src/main/java/com/example/kbasics/basics.kt
@@ -0,0 +1,8 @@
+package com.example.kbasics
+
+fun main(){
+    vaL myName = "Kristijan"
+    myName ="Kristijan"
+    print("pozzdrav " + myName)
+}
+    //tipovi podataka se ne moraju specificirati
