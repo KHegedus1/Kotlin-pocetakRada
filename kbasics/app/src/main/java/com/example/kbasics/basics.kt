5db2a8a009435ec8ede273cfe04e8f09828c669c
KHegedus1 <kico.kiki@gmail.com>
2022-04-28 23:51:37 +0200

    aritmeticke operacije

--- kbasics/app/src/main/java/com/example/kbasics/basics.kt
+++ kbasics/app/src/main/java/com/example/kbasics/basics.kt
@@ -3,6 +3,25 @@ package com.example.kbasics
 fun main(){
     vaL myName = "Kristijan"
     myName ="Kristijan"
-    print("pozzdrav " + myName)
+            print("pozzdrav " + myName)
+
+    var result = 5+3
+    result /=2
+            print(result)
+
+    var result:  = 5+3
+    result  = 15%2
+            print(result)
+
+    val a=5
+    val b=3
+    result = a/b
+            print(result)
+    var resultDouble : Double
+    val a=5.0
+    val b=3
+    resultDouble =  a/b
+            print(resultDouble)
 }
     //tipovi podataka se ne moraju specificirati
+
