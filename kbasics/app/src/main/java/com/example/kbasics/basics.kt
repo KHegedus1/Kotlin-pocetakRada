4e268fdffe38b08f4df366ca529978d490e5b84a
KHegedus1 <kico.kiki@gmail.com>
2022-05-06 16:50:38 +0200

    continue

--- kbasics/app/src/main/java/com/example/kbasics/basics.kt
+++ kbasics/app/src/main/java/com/example/kbasics/basics.kt
@@ -130,10 +130,10 @@ fun main(){
 
     }
     for( i in 1 until 20){
-        print("$i ")
         if(i/2 ==5){
-            break
+            continue
         }
+        print("$i ")
     }
     print("done with the loop!")
 }
