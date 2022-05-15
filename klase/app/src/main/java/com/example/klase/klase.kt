6601b7d1271f14b2d50368adb2ec69b4d4c32d68
KHegedus1 <kico.kiki@gmail.com>
2022-05-15 15:51:31 +0200

    automatski se poziva john

--- klase/app/src/main/java/com/example/klase/klase.kt
+++ klase/app/src/main/java/com/example/klase/klase.kt
@@ -2,8 +2,9 @@ package com.example.klase
 
 fun main (){
     var kh = Person("Kristijan","sdssasd")
+    var john = Person()
 }
-class Person constructor(firstName: String, lastName: String){
+class Person constructor(firstName: String = "John", lastName: String = "Doe"){
     init{
         println("Initialized a new person object with " + "Firstname= $firstName and Lastname= $lastName")
     }
