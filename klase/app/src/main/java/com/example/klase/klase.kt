fc793a409d491dd633c42097007ccf5d553382be
KHegedus1 <kico.kiki@gmail.com>
2022-05-21 16:59:59 +0200

    Scope and shadowing

--- klase/app/src/main/java/com/example/klase/klase.kt
+++ klase/app/src/main/java/com/example/klase/klase.kt
@@ -1,12 +1,18 @@
 package com.example.klase
 
 fun main (){
+    myFunction(a:5)
     var kh = Person("Kristijan","sdssasd")
     var john = Person()
     var johnPeterson = Person(lastName = "Peterson")
 }
+
 class Person constructor(firstName: String = "John", lastName: String = "Doe"){
     init{
         println("Initialized a new person object with " + "Firstname= $firstName and Lastname= $lastName")
     }
-}
+
+    fun myFunction(a:Int){
+         var a = 5
+
+    }    }
