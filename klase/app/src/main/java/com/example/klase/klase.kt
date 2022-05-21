834837f3fbc161b7d1af359583a9051e9749d7d6
KHegedus1 <kico.kiki@gmail.com>
2022-05-21 17:06:50 +0200

    komentar-shadowing

--- klase/app/src/main/java/com/example/klase/klase.kt
+++ klase/app/src/main/java/com/example/klase/klase.kt
@@ -11,9 +11,9 @@ class Person constructor(firstName: String = "John", lastName: String = "Doe"){
     init{
         println("Initialized a new person object with " + "Firstname= $firstName and Lastname= $lastName")
     }
-
+}
     fun myFunction(a:Int){ //parametar
          var a = 5
         println("a is $a")
 
-    }    }
+    }    //Shadowing-Kotlin dopusta deklariranje varijable istog imena kao sto ga ima i parametar.
