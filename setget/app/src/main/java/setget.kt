b6d22bc2d8f5cffd0cbf5949c201f16b0daa846f
KHegedus1 <kico.kiki@gmail.com>
2022-05-22 15:57:36 +0200

    init,custom getter

--- setget/app/src/main/java/setget.kt
+++ setget/app/src/main/java/setget.kt
@@ -1,6 +1,17 @@
 fun main(){
-
+var myCar = Car()
+    myCar.Owner
+    println ("brand is: ${myCar.myBrand}")
 }
 class Car(){
     lateinit var Owner : string //lateinit-kasnije cemo initializirati var
+
+    val myBrand: String = "BMW"
+        //custom getter  
+    get() {
+        return field.toLowerCase()
+    }
+    init{
+        Owner = "Frank"
+    }
 }
