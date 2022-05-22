efaa7f36c6a16abc1ed5d2895d7779726268ddd5
KHegedus1 <kico.kiki@gmail.com>
2022-05-22 16:04:45 +0200

    setteri,jednostavni kontroler

--- setget/app/src/main/java/setget.kt
+++ setget/app/src/main/java/setget.kt
@@ -2,15 +2,24 @@ fun main(){
 var myCar = Car()
     myCar.Owner
     println ("brand is: ${myCar.myBrand}")
+    myCar.maxSpeed= 200 //Set
+    println ("Max speed is is: ${myCar.maxSpeed}")
+
 }
 class Car(){
     lateinit var Owner : string //lateinit-kasnije cemo initializirati var
 
     val myBrand: String = "BMW"
-        //custom getter  
+        //custom getter
     get() {
         return field.toLowerCase()
     }
+    var maxSpeed : Int = 250
+   // get() = field
+    set(value){
+        field= if(value>0)value else throw IllegalArgumentException("Maxspeed can't be less than 0:")
+    }
+
     init{
         Owner = "Frank"
     }
