5400ace1e6814e1aeba2ac22d013952718f9c265
KHegedus1 <kico.kiki@gmail.com>
2022-05-22 16:08:05 +0200

    privage set

--- setget/app/src/main/java/setget.kt
+++ setget/app/src/main/java/setget.kt
@@ -4,7 +4,7 @@ var myCar = Car()
     println ("brand is: ${myCar.myBrand}")
     myCar.maxSpeed= 200 //Set
     println ("Max speed is is: ${myCar.maxSpeed}")
-
+    println ("Model is ${myCar.myModel}")
 }
 class Car(){
     lateinit var Owner : string //lateinit-kasnije cemo initializirati var
@@ -20,7 +20,10 @@ class Car(){
         field= if(value>0)value else throw IllegalArgumentException("Maxspeed can't be less than 0:")
     }
 
+    var myModel = "M5"
+    private set//private-dostupan samo unutar ove klase
     init{
+        this.myModel = "M3"
         Owner = "Frank"
     }
 }
