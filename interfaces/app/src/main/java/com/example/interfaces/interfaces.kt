b770b49470277084a53f959a2e738efbebd2e42f
KHegedus1 <kico.kiki@gmail.com>
2022-05-28 18:23:51 +0200

    za naslijedjivanje trebamo otvoriti klasu

---
+++ interfaces/app/src/main/java/com/example/interfaces/interfaces.kt
@@ -0,0 +1,13 @@
+package com.example.interfaces
+
+open class Vehicle{ //klasa mora biti otvorena da bi se dogodilo naslijedjivanje
+    //properties
+    //methods
+}
+
+class Car : Vehicle(){
+
+}
+class  ElectricCar: Car(){
+    
+}
