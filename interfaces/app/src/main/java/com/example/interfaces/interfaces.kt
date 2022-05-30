dc6143c1385ba9bef6de32f8f5756eca4a9493bc
KHegedus1 <kico.kiki@gmail.com>
2022-05-30 14:20:19 +0200

    override

--- interfaces/app/src/main/java/com/example/interfaces/interfaces.kt
+++ interfaces/app/src/main/java/com/example/interfaces/interfaces.kt
@@ -12,11 +12,18 @@ class  ElectricCar: Car(){
 }
 
 //Base class-super class
-vopen class Car(val name: String,val brand: String){
+    open class Car(val name: String,val brand: String){
     open var range: Double = 0.0
     fun extendRange(amount: Double){
         if (amount  > 0)
             range += amount
+    }
+    class ElectricCar(name : String, brand : String, batteryLife : Double)
+        : Car (name,brand){
+    override var range = batteryLife * 6
+    override fun drive (distance: Double){
+        println(" Drove for $distance KM"
+    }
     }
 }
 
