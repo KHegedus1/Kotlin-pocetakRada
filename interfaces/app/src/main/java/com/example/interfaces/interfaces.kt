ebf4c61884b9fbf4265f3b1f5849668f5bc75db7
KHegedus1 <kico.kiki@gmail.com>
2022-05-30 14:15:30 +0200

    superclass

--- interfaces/app/src/main/java/com/example/interfaces/interfaces.kt
+++ interfaces/app/src/main/java/com/example/interfaces/interfaces.kt
@@ -9,5 +9,14 @@ class Car : Vehicle(){
 
 }
 class  ElectricCar: Car(){
-    
 }
+
+//Base class-super class
+vopen class Car(val name: String,val brand: String){
+    open var range: Double = 0.0
+    fun extendRange(amount: Double){
+        if (amount  > 0)
+            range += amount
+    }
+}
+
