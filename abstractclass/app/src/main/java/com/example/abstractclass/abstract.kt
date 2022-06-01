b7d686b73868e20662e0f0f3a0cf1603d25ea0a3
KHegedus1 <kico.kiki@gmail.com>
2022-06-01 15:35:06 +0200

    abstract properties

--- abstractclass/app/src/main/java/com/example/abstractclass/abstract.kt
+++ abstractclass/app/src/main/java/com/example/abstractclass/abstract.kt
@@ -1,2 +1,7 @@
 package com.example.abstractclass
 
+abstract class Mammal(private val name: String, private val origin: String,
+                      private val weight: Double){
+    //abstract properties(must be overriden by subclasses)
+    abstract var maxSpeed: Double
+}
